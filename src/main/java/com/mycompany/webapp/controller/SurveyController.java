ab636d498ce3122f372c6e09924a2003937e92f6
Jin Lee <110071055+Jinlunalee@users.noreply.github.com>
2022-12-27 13:29:03 +0900

    DESIGN : survey CSS 추가, register insert 버튼 디자인 변경, 왼쪽 서브메뉴 추가

--- src/main/java/com/mycompany/webapp/controller/SurveyController.java
+++ src/main/java/com/mycompany/webapp/controller/SurveyController.java
@@ -1,31 +1,51 @@
 package com.mycompany.webapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mycompany.webapp.dto.SurveyVO;
 
 @Controller
 public class SurveyController {
 	
+	//테스트
+	@RequestMapping("/survey/details/")
+	public String detailsTest(Model model) {
+		model.addAttribute("menu", "survey");
+		model.addAttribute("menuKOR", "만족도 조사 관리");
+		return "survey/details";
+	}
+	@RequestMapping("/survey/update/")
+	public String updateTest(Model model) {
+		model.addAttribute("menu", "survey");
+		model.addAttribute("menuKOR", "만족도 조사 관리");
+		return "survey/update";
+	}
+	
 	//목록조회
 	@RequestMapping(value="/survey/list", method=RequestMethod.GET)
-	public String getSurveyList() {
+	public String getSurveyList(Model model) {
+		model.addAttribute("menu", "survey");
+		model.addAttribute("menuKOR", "만족도 조사 관리");
 		return "survey/list";
 	}
 
 	//상세조회
 	@RequestMapping(value="/survey/details/{surveyId}", method=RequestMethod.GET)
-	public String getSurveyDetails(@PathVariable String surveyId) {
+	public String getSurveyDetails(@PathVariable String surveyId, Model model) {
+		model.addAttribute("menu", "survey");
+		model.addAttribute("menuKOR", "만족도 조사 관리");
 		return "survey/details";
 	}
 
 	//수정
 	@RequestMapping(value="/survey/update/{surveyId}", method=RequestMethod.GET)
-	public String updateSurvey(@PathVariable String surveyId) {
+	public String updateSurvey(@PathVariable String surveyId, Model model) {
+		model.addAttribute("menu", "survey");
+		model.addAttribute("menuKOR", "만족도 조사 관리");
 		return "survey/update";
 	}
 
@@ -42,7 +62,9 @@ public class SurveyController {
 
 	//입력
 	@RequestMapping(value="/survey/insert", method=RequestMethod.GET)
-	public String insertSurvey() {
+	public String insertSurvey(Model model) {
+		model.addAttribute("menu", "survey");
+		model.addAttribute("menuKOR", "만족도 조사 관리");
 		return "survey/insert";
 	}
 
@@ -53,7 +75,9 @@ public class SurveyController {
 	
 	//통계
 	@RequestMapping(value="/survey/summary/{courseId}", method=RequestMethod.GET)
-	public String getSurveySummary(@PathVariable String courseId) {
+	public String getSurveySummary(@PathVariable String courseId, Model model) {
+		model.addAttribute("menu", "survey");
+		model.addAttribute("menuKOR", "만족도 조사 관리");
 		return "survey/summary";
 	}
 }
--- src/main/webapp/WEB-INF/views/common/menu.jsp
+++ src/main/webapp/WEB-INF/views/common/menu.jsp
@@ -1,20 +1,9 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
 
-<ul class="nav flex-column">
-	<li class="nav-item mb-2">
-		<h6 class="text-white">
-			<span class="mr-2">Title.</span>
-			<a class="text-white" href="#">Subtitle</a>
-			<ul>
-				<li><a href="#">과정관리</a></li>
-				<li>
-				<a class="text-white" href="<c:url value='/course/regularlist'/>">과정목록</a>
-				</li>
-				<li><a class="text-white" href="<c:url value='/register/list'/>">수강목록</a></li>
-				<li><a class="text-white" href="<c:url value='/student/list'/>">수강생목록</a></li>
-				<li><a class="text-white" href="<c:url value='/survey/list'/>">강의 만족도 조사 목록</a></li>
-				<li><a class="text-white" href="<c:url value='/data/download'/>">연계 자료 다운</a></li>
-			</ul>
-		</h6>
-	</li>	
-</ul>
+<h6 class="text-white">
+	<div class="aside-bar">
+		<div class="aside-bar-menu">${menuKOR}</div>
+		<div class="aside-bar-submenu">
+		</div>
+	</div>
+</h6>
