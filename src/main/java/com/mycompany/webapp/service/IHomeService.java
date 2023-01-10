0e5140195f0be883d5ba45ca7bc0fd974d146c80
한혜원 <hw03822@gmail.com>
2023-01-10 17:52:09 +0900

    DOCS : Home 문서 생성

---
+++ src/main/java/com/mycompany/webapp/service/IHomeService.java
@@ -0,0 +1,5 @@
+package com.mycompany.webapp.service;
+
+public interface IHomeService {
+
+}
---
+++ src/main/java/com/mycompany/webapp/service/impl/HomeService.java
@@ -0,0 +1,7 @@
+package com.mycompany.webapp.service.impl;
+
+import com.mycompany.webapp.service.IHomeService;
+
+public class HomeService implements IHomeService {
+
+}
