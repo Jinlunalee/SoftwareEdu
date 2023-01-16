f69be635e4f27d29554b53201cf8c1ee1b8202d3
어민수 <zlqjcwz@gmail.com>
2023-01-16 10:58:46 +0900

    FEAT : 수강 버튼

--- src/main/java/com/mycompany/webapp/service/impl/PagerService.java
+++ src/main/java/com/mycompany/webapp/service/impl/PagerService.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-import org.omg.CORBA.BAD_INV_ORDER;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
