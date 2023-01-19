59ba6534bf7a0a83096f2ec8275fa22177cc8b00
EUNSOO <qurrdorr@gmail.com>
2023-01-19 19:06:59 +0900

    FIX : 데이터연계자료 4번째 조회 수정

--- src/main/java/com/mycompany/webapp/controller/DataController.java
+++ src/main/java/com/mycompany/webapp/controller/DataController.java
@@ -108,7 +108,7 @@ public class DataController {
 	@RequestMapping(value="/getjsonSbj", method=RequestMethod.GET, produces = "application/text; charset=UTF-8" )
 	public @ResponseBody String getjsonSbj(@RequestParam("sDay") String startDay, @RequestParam("eDay") String endDay){
 		
-		String resultJsonSbjStr = dataService.getDataGson(startDay, endDay);
+		String resultJsonSbjStr = dataService.getSbjDataGson(startDay, endDay);
 		
 		return resultJsonSbjStr;
 	}	
@@ -117,19 +117,14 @@ public class DataController {
 	
 	
 // 수정중 2023.01.19  오후 2시 30분 
-	@RequestMapping(value="/getxmlSbj", method=RequestMethod.GET, produces = "application/text; charset=UTF-8")
+	@RequestMapping(value="/getxmlSbj", method=RequestMethod.GET, produces = "application/json; charset=UTF-8")
 	public @ResponseBody List<SubjectVO> getxmlSbj(SubjectVO subjectVo, @RequestParam("sDay") String startDay, @RequestParam("eDay") String endDay) {
-		
-		
-		
-		
-		startDay.replaceAll("-", "");
-		endDay.replaceAll("-", "");
-		
+	
 		List<SubjectVO> sbjDataList = dataService.getSbjDataList(startDay, endDay);
 		
 //		String resultXmlStr = "";
-						System.out.println("controller : " + sbjDataList);
+		System.out.println("controller : " + sbjDataList);
+		
 		return sbjDataList;
 	}
 }
