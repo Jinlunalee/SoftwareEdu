b5cc1370ed0974d14d4a162de8340cadc1539295
어민수 <zlqjcwz@gmail.com>
2023-01-25 11:32:04 +0900

    DOCS : EnrollVO 주석

--- src/main/java/com/mycompany/webapp/dto/EnrollVO.java
+++ src/main/java/com/mycompany/webapp/dto/EnrollVO.java
@@ -13,26 +13,26 @@ public class EnrollVO {
 	private String subjectId;			// 강좌 아이디
 	private int subjectSeq;				// 강좌 시퀀스
 	private String studentId;			// 수강생 아이디
-	private String stateCd;				// 수강 상태 공통 코드
-	private int completeHours;			// 수강 완료 시간
+	private String stateCd;				// 수강 상태 코드
+	private int completeHours;			// 수강 완료 시수
 	private String enrollDt;			// 수강 신청 일시
-	private String regId;				// 수강 등록 아이디
-	private String regDt;				// 수강 등록 일자
-	private String modiId;				// 수강 변경 아이디
+	private String regId;				// 수강 등록자
+	private String regDt;				// 수강 등록 일시
+	private String modiId;				// 수강 변경자
 	private String modiDt;				// 수강 변경 일시
 	private String delYn;				// 수강 삭제 여부
-	private String cancelRsCd;			// 수강 취소 공통 코드
-	private String cancelRsEtc;			// 수강 취소 기타 사유
+	private String cancelRsCd;			// 수강 취소 사유 코드
+	private String cancelRsEtc;			// 수강 취소 사유 기타
 	
 	private int ratio;					// 수강 진도율
 	private int addHours;				// 수강 추가 시간
 	
 	// 강좌
 	private String subjectTitle;		// 강좌 명
-	private String catSubjectCd;		// 강좌 분류 공통 코드
+	private String catSubjectCd;		// 강좌 분류 코드
 	private int days;					// 강좌 일수
 	private int hours;					// 강좌 시수
-	private String levelCd;				// 강좌 난이도 공통 코드
+	private String levelCd;				// 강좌 난이도 코드
 	
 	// 과정
 	private String courseId;			// 과정 아이디
@@ -47,17 +47,17 @@ public class EnrollVO {
 	private String recruitEndDay;		// 개설 강좌 모집 종료 일자
 	private String recruitPeople;		// 개설 강좌 모집 인원
 	
-	private String openStateCd;			// 개설 강좌 상태 공통 코드
+	private String openStateCd;			// 개설 강좌 상태 코드
 	
 	// 수강생
 	private String name;				// 수강생 명
 	private String birth;				// 수강생 생년월일
-    private String genderCd;			// 수강생 성별 공통 코드
+    private String genderCd;			// 수강생 성별 코드
     private String email;				// 수강생 이메일
     private String phone;				// 수강생 전화번호
-    private String addDoCd;				// 수강생 주소 공통 코드
+    private String addDoCd;				// 수강생 주소 코드
     private String addEtc;				// 수강생 주소 기타
-    private String positionCd;			// 수강생 구분 공통 코드
+    private String positionCd;			// 수강생 구분 코드
     
     // 검색
     private String applyStartDay;		// 신청 시작 일자
@@ -69,13 +69,13 @@ public class EnrollVO {
 	private String keyword2;			// 강좌 과정 명 입력
     
     // 공통
-	private String stateCdTitle;		// 수강 상태 공통 코드명
-	private String cancelRsTitle;		// 수강 취소 공통 코드명
-	private String catSubjectCdTitle;	// 강좌 분류 공통 코드명
-	private String levelCdTitle;		// 강좌 난이도 공통 코드명
-	private String openStateCdTitle;	// 개설 강좌 상태 공통 코드명
-	private String genderCdTitle;		// 수강생 성별 공통 코드명
-	private String addDoCdTitle;		// 수강생 주소 공통 코드명
-	private String positionCdTitle;		// 수강생 구분 공통 코드명
+	private String stateCdTitle;		// 수강 상태 코드 명
+	private String cancelRsTitle;		// 수강 취소 코드 명
+	private String catSubjectCdTitle;	// 강좌 분류 코드 명
+	private String levelCdTitle;		// 강좌 난이도 코드 명
+	private String openStateCdTitle;	// 개설 강좌 상태 코드 명
+	private String genderCdTitle;		// 수강생 성별 코드 명
+	private String addDoCdTitle;		// 수강생 주소 코드 명
+	private String positionCdTitle;		// 수강생 구분 코드 명
 	
 }
