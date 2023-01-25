7db02c4561508dab32a95a07159bb8b5c9457f55
EUNSOO <qurrdorr@gmail.com>
2023-01-25 21:53:25 +0900

    FIX : @Expose 항목 추가

--- src/main/java/com/mycompany/webapp/dto/OpenVO.java
+++ src/main/java/com/mycompany/webapp/dto/OpenVO.java
@@ -15,15 +15,18 @@ public class OpenVO {
 	//강좌 관련
 	private String subjectId; //강좌아이디
 	private int subjectSeq; //강좌시퀀스
+	@Expose
 	private String subjectTitle; //강좌명
 	private String supportYn; //지원여부
 	private String catSubjectCd;//강좌 분류 코드
 	private String catSubjectCdTitle;//강좌 분류 코드명
 	private int days; //일수
+	@Expose
 	private int hours; //시수
 	private String levelCd; //레벨 코드
 	private String levelCdTitle; // 레벨 코드명
 	private String levelEtc; // 기타
+	@Expose
 	private int cost; //수강비
 	private int regYear;//등록년도
 	
