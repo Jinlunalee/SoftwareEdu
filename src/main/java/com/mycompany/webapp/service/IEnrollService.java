a59425f648c5382db6fd6eb66d6b73e8f5495134
어민수 <zlqjcwz@gmail.com>
2023-02-15 20:57:05 +0900

    DOCS : 수강 서비스

--- src/main/java/com/mycompany/webapp/service/IEnrollService.java
+++ src/main/java/com/mycompany/webapp/service/IEnrollService.java
@@ -8,17 +8,17 @@ import com.mycompany.webapp.dto.OpenVO;
 import com.mycompany.webapp.dto.StudentVO;
 
 public interface IEnrollService {
-	void clickCancel(EnrollVO enroll, String studentId, String subjectId, int subjectSeq);
-	void clickDelete(String studentId, String subjectId, int subjectSeq);
-	int getRatioUsingEnrollId(String enrollId);
+	void clickCancel(EnrollVO enroll, String studentId, String subjectId, int subjectSeq); // 취소 버튼
+	void clickDelete(String studentId, String subjectId, int subjectSeq); // 논리 삭제
+	int getRatioUsingEnrollId(String enrollId); // 진도율
 	void clickDeleteEnrollByOpen(String subjectId, int subjectSeq);
-	void addHours(int addHours, String enrollId);
-	List<CommonCodeVO> getCancelList();
-	List<StudentVO> getStudentList(StudentVO studentVO);
-	void approval(String studentId, String subjectId, int subjectSeq);
-	List<OpenVO> getOpenList(OpenVO openVO);
-	void addEnroll(String studentId, String subjcetId, int subjectSeq);
-	void addCourse(String studentId, String courseId, String courseOpenYear);
-	int recruitTotalPeople(String subjectId, int subjectSeq, String state);
-	EnrollVO getEnrollDetails(String enrollId);
+	void addHours(int addHours, String enrollId); // 수강 시간 입력
+	List<CommonCodeVO> getCancelList(); // 취소 사유 리스트
+	List<StudentVO> getStudentList(StudentVO studentVO); // 수강생 리스트
+	void approval(String studentId, String subjectId, int subjectSeq); // 승인 버튼
+	List<OpenVO> getOpenList(OpenVO openVO); // 
+	void addEnroll(String studentId, String subjcetId, int subjectSeq); // 수강 강좌 추가
+	void addCourse(String studentId, String courseId, String courseOpenYear); // 수강 과정 추가
+	int recruitTotalPeople(String subjectId, int subjectSeq, String state); // 모집인원
+	EnrollVO getEnrollDetails(String enrollId); // 수강 상세 화면
 }
